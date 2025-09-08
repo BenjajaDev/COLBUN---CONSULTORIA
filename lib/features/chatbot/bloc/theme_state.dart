c8b879c1661810943dc81cbce35193fe27f7f4a9
luttidano <luciano.valenzuela@cloud.uautonoma.cl>
2025-09-08 16:11:19 -0300

    Bloc integrado y switch oscuro/claro

---
+++ lib/features/chatbot/bloc/theme_state.dart
@@ -0,0 +1,23 @@
+part of 'theme_bloc.dart';
+
+class ThemeState {
+  final bool isDarkMode;
+
+  const ThemeState({required this.isDarkMode});
+
+  ThemeState copyWith({bool? isDarkMode}) {
+    return ThemeState(
+      isDarkMode: isDarkMode ?? this.isDarkMode,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ThemeState && other.isDarkMode == isDarkMode;
+  }
+
+  @override
+  int get hashCode => isDarkMode.hashCode;
+}
