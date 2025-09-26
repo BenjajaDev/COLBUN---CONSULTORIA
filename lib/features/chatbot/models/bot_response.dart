7a2c21bf6e1b75816eed690d9549070b8d7d30be
Lushiiooo2 <josejose214376@gmail.com>
2025-09-25 21:13:12 -0300

    firestore_faq_service obtener faqs de firebase

--- lib/features/chatbot/models/bot_response.dart
+++ lib/features/chatbot/models/bot_response.dart
@@ -1,11 +1,14 @@
-
 class BotResponse {
   final String answer;
   final String? action; // El action es opcional
-  final bool isStandardResponse; // Indica si es una respuesta estándar que requiere feedback
+  final bool isStandardResponse;
+  final String?
+      source; // Indica si es una respuesta estándar que requiere feedback
 
-  BotResponse({required this.answer,
-  this.action,
-  this.isStandardResponse = true
+  BotResponse({
+    required this.answer,
+    this.action,
+    this.isStandardResponse = true,
+    this.source,
   });
 }
