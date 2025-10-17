b3af79bbbf24c51a364e07f920dd416226246240
Benji <jesuscaballerodaza@gmail.com>
2025-10-17 19:07:42 -0300

    feat: boton de emergencia

---
+++ lib/screens/emergency_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class EmergencyScreen extends StatelessWidget {
+  const EmergencyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Emergencias'),
+      ),
+      body: const Center(
+        child: Text('Pantalla de emergencia'),
+      ),
+    );
+  }
+}
+
